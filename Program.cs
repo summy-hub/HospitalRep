62c7a4698854f48d1d5e0d48903a8326902d2120
codedcozy12 <khalismowiy04@gmail.com>
2025-03-04 16:02:21 +0100

    first commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter first number");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second number");
+int secondNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter third number");
+int thirdNumber = int.Parse(Console.ReadLine());
+Console.WriteLine(AddThreeIntegers(firstNumber, secondNumber, thirdNumber));
+static int AddThreeIntegers(int firstNumber, int secondNumber, int thirdNumber)
+{
+    return firstNumber + secondNumber + thirdNumber;
+}
